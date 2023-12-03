affb6b1a42ca633e19260398bc6fa4de939222fe
T. Todua <7117978+ttodua@users.noreply.github.com>
2023-12-03 17:35:09 +0400

    add exerciseOptions from interface

--- src/IB.CSharpApiClient/IBClientSocket.cs
+++ src/IB.CSharpApiClient/IBClientSocket.cs
@@ -7,5 +7,10 @@ namespace IB.CSharpApiClient
         public IBClientSocket(EWrapper wrapper, EReaderSignal eReaderSignal) : base(wrapper, eReaderSignal)
         {
         }
+
+        public void exerciseOptions(int tickerId, Contract contract, int exerciseAction, int exerciseQuantity, string account, int ovrd)
+        {
+            throw new System.Exception("Not implemented");
+        }
     }
 }
